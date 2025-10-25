438447b710a2c2ecc6c2457d3e54c16effbb0619
ivsjsc <ivscorp.vn@gmail.com>
2025-10-26 04:00:38 +0700

    commit

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import { Utinities, ListOA } from "@components";
 import PageLayout from "@components/layout/PageLayout";
 import { APP_UTINITIES } from "@constants/utinities";
+import WelcomeBanner from "@components/WelcomeBanner";
 import Contacts from "./Contacts";
 import Procedures from "./Procedures";
-import WelcomeBanner from "@components/WelcomeBanner";
 
-const HomePage: React.FunctionComponent = () => {
-    return (
-        <PageLayout id="home-page">
-            <WelcomeBanner />
-            <Utinities utinities={APP_UTINITIES} />
-            <ListOA />
-            <Contacts />
-            <Procedures />
-        </PageLayout>
-    );
-};
+const HomePage: React.FunctionComponent = () => (
+    <PageLayout id="home-page">
+        <WelcomeBanner />
+        <Utinities utinities={APP_UTINITIES} />
+        <ListOA />
+        <Contacts />
+        <Procedures />
+    </PageLayout>
+);
 
 export default HomePage;
